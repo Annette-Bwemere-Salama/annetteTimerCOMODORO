0b98fd61e609758a631eb740c65c8c9542bcce96
annette Bwemere <bwemereannette@gmail.com>
2022-08-11 16:12:38 +0200

    feat: ajout de la grande session du timer munite seconde

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 
+// import "App.js";
 import "./App.css";
-import BreakLength from "BreakLength";
+import BreakLength from "./COMPONENT/BreakLength.js";
+// import "App.js";
 /*
 import  Header from "./COMPONENT/Header.js";
 import Main from "./COMPONENT/Clock.js";
 import Mainsection from "./COMPONENT/Mainsection.js";
 import FooterIcons from "./COMPONENT/Footer.js";*/
 import Clock from "./COMPONENT/Clock.js";
+import Session from "./COMPONENT/session";
 
 function App() {
   return (
-    <div className="diveDensemble">
-      <Clock />
+    <div>
+      <Clock />,
       <BreakLength />
+      <Session />,
     </div>
   );
 }
---
+++ src/COMPONENT/session.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Session() {
+  return (
+    <div>
+      <p>Session</p>
+      <h3>24:00</h3>
+    </div>
+  );
+}
+
+export default Session;
